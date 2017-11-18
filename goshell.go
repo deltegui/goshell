5f79f7b0b2ad02da2c26b300f138ce79f619f503
frikiman34 <frikiman34@hotmail.com>
2017-11-18 13:12:08 +0100

    refactor for introduce testing

--- goshell.go
+++ goshell.go
@@ -2,7 +2,6 @@
 package goshell
 
 import (
-	"fmt"
 	"errors"
 	"bufio"
 	"strings"
@@ -24,24 +23,32 @@ type Shell struct {
 	prompt string
 	exitSequence string
 	commands map[string]CommandHandler
+	reader *bufio.Scanner
+	writer *bufio.Writer
 }
 
 // NewShell creates a custom shell with a prompt and a exitSequence.
 // The exitSequence is a string that the user should write in the cli
 // to exit.
-func NewShell(prompt string, exitSequence string) Shell {
+func NewShell(prompt string, exitSequence string, writer *os.File, reader *os.File) Shell {
 	s := Shell{
 		prompt: prompt,
 		commands: make(map[string]CommandHandler),
 		exitSequence: exitSequence,
+		reader: bufio.NewScanner(reader),
+		writer: bufio.NewWriter(writer),
 	}
 	s.RegistrerCommand("command-list", s.listCommands)
 	return s
 }
 
-// NewDefaultShell creates a new shell with default options.
+
+// NewDefaultShell creates a new shell with default options:
+//	- Prompt is '~> '
+//	- The exit command is 'exit'
+//	- The default io is stdout and stdin 
 func NewDefaultShell() Shell {
-	return NewShell("~>", "exit")
+	return NewShell("~> ", "exit", os.Stdout, os.Stdin)
 }
 
 // RegistrerCommand registrers a new command. You should pass a command name
@@ -58,6 +65,33 @@ func(shell *Shell) RegistrerAllCommands(newCommands map[string]CommandHandler) {
 	}
 }
 
+// Run starts a your cli. You pass your shell strcture and a channel to
+// notify when the cli exited. If you are not using goroutines, you can
+// pass nil.
+func(shell Shell) Run(endNotification chan<- bool) {
+	var (
+		commandName string
+		parameters []string
+	)
+	defer close(endNotification)
+	for {
+		shell.writeLine(shell.prompt)
+		commandName, parameters = shell.readCommand()
+		if commandName == shell.exitSequence {
+			break
+		}
+		if len(commandName) > 0 {
+			err := shell.dispatchCommand(commandName, parameters)
+			if err != nil {
+				shell.writeLine(err.Error() + "\n")
+			}
+		}
+	}
+	if(endNotification != nil) {
+		endNotification <- true
+	}
+}
+
 func(shell Shell) dispatchCommand(target string, params []string) error {
 	for name, handler := range(shell.commands) {
 		if(name == target) {
@@ -67,45 +101,21 @@ func(shell Shell) dispatchCommand(target string, params []string) error {
 	return errCommandMissing
 }
 
-// Run starts a your cli. You pass your shell strcture and a channel to
-// notify when the cli exited. If you are not using goroutines, you can
-// pass nil.
-func Run(shell Shell, endNotification chan<- bool) {
-	var (
-		commandName string
-		parameters []string
-		scanner *bufio.Scanner
-	)
-	defer close(endNotification)
-	scanner = bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Printf("%s ", shell.prompt)
-		commandName, parameters = readCommand(scanner)
-		if commandName == shell.exitSequence {
-			break
-		}
-		if len(commandName) > 0 {
-			err := shell.dispatchCommand(commandName, parameters)
-			if err != nil {
-				fmt.Println(err)
-			}
-		}
-	}
-	if(endNotification != nil) {
-		endNotification <- true
-	}
-}
-
-func readCommand(scanner *bufio.Scanner) (string, []string) {
+func(shell Shell) readCommand() (string, []string) {
 	var (
 		line string
 		parts []string
 	)
-	scanner.Scan()
-	line = scanner.Text()
+	shell.reader.Scan()
+	line = shell.reader.Text()
 	parts = strings.Split(line, " ")
 	if len(parts) > 0 {
 		return parts[0], parts[1:]
 	}
 	return "", nil
+}
+
+func(shell Shell) writeLine(line string) {
+	shell.writer.WriteString(line)
+	shell.writer.Flush()
 }
